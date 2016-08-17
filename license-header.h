0e5c98483fbc475c548f10c5eecf6b0af108aec0
ugo <ugovaretto@gmail.com>
2016-08-17 10:29:56 +0200

    Fix license header

--- license-header.h
+++ license-header.h
@@ -12,5 +12,5 @@
 //GNU General Public License for more details.
 //
 //You should have received a copy of the GNU General Public License
-//along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//along with tjpp.  If not, see <http://www.gnu.org/licenses/>.
 
